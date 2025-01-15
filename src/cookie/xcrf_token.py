35d754f2c66f4dcf137d5e2ca0a6ce0fc0159e1f
efe <113731512+efenatuyo@users.noreply.github.com>
2025-01-15 18:06:28 +0100

    Update xcrf_token.py

--- src/cookie/xcrf_token.py
+++ src/cookie/xcrf_token.py
@@ -2,7 +2,7 @@ import aiohttp
 
 async def get(self):
     session = aiohttp.ClientSession(connector=aiohttp.TCPConnector(limit=None), cookies={".ROBLOSECURITY": self.cookie}, headers={'Accept-Encoding': 'gzip, deflate'})
-    response = (await session.post("https://auth.roblox.com/v2/logout")).headers.get("x-csrf-token")
+    response = (await session.post("https://catalog.roblox.com/v1/catalog/items/details")).headers.get("x-csrf-token")
     await session.close()
     assert response, "Failed to get x-csrf-token"
     return response
